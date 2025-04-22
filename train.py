758d21641e67c31e30c2d08c129db4953da219b7
Nguyen Van Thanh Dat <dat.nvt225606@sis.hust.edu.vn>
2025-04-22 13:04:25 +0700

    1

--- train.py
+++ train.py
@@ -75,8 +75,8 @@ print("🚀 Training bắt đầu lúc:", time.strftime('%Y-%m-%d %H:%M:%S'))
 training_args = TrainingArguments(
     output_dir='./results',
     num_train_epochs=3,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
     warmup_steps=500,
     weight_decay=0.01,
     logging_dir='./logs',
